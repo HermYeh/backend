006c2b3b8ce93e1a26340a6e6ee72b8d151af9e7
HermYeh <qsoya@hotmail.com>
2024-05-21 00:17:32 -0400

    ll

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
 
+use log::info;
 use sqlite::{self, Row};
 use std::net::{Shutdown,TcpListener, TcpStream};
 use std::{
     io::{ErrorKind, Read, Write},
-    thread
+  
 };
+use warp::Filter;
+
+use std::convert::Infallible;
+use env_logger;
+#[macro_use]
+use log;
+
 use std::mem::size_of;
-use std::error::Error;
+
 use serde::{Deserialize, Serialize};
 fn handle_connection( vector:Vec<(String,String,bool)>) {
        
        
         let connection = sqlite::Connection::open("order.db").unwrap();
-        let query = "CREATE TABLE IF NOT EXISTS data (order_number TEXT PRIMARY KEY, check_in TEXT);";
+        let mut query = "CREATE TABLE IF NOT EXISTS data (order_number TEXT PRIMARY KEY, check_in TEXT);";
         println!("query{}",query);
         connection.execute(query).unwrap();
         
+        query = "DELETE FROM data;";
+        connection.execute(query).unwrap();
         for each in vector.iter(){
             let query="INSERT OR IGNORE INTO data (order_number, check_in) VALUES ('".to_owned()+&each.0+"','"+&each.1+"');
             UPDATE data SET check_in ='"+&each.1+"' WHERE order_number='"+&each.0+"';";
@@ -42,6 +52,7 @@ struct FileHeader {
     size: u32,
     
 }
+use pretty_env_logger;
 #[derive(Serialize, Deserialize)]
 struct Orders{
 order_number:String,
@@ -61,9 +72,7 @@ fn handle_client(mut stream: TcpStream)->  std::io::Result<()> {
     let file_size = file_header.size as usize;
     let mut buf = [0; BUF_LEN];
     println!("file_header.size{:?}",file_header.size);
-    
-    if file_size==0{
-    let conn =sqlite::Connection::open("order.db").unwrap();
+       let conn =sqlite::Connection::open("order.db").unwrap();
        let query = "SELECT * FROM data";
     let mut statement=conn.prepare(query).unwrap();
      while let sqlite::State::Row= statement.next().unwrap(){
@@ -72,6 +81,8 @@ fn handle_client(mut stream: TcpStream)->  std::io::Result<()> {
      let checkin=statement.read(1).unwrap();
      total_orders.push((orderid,checkin,false));
      } ;
+    if file_size==0{
+ 
      let message = Message { vector:total_orders};
     
      let data = serde_json::to_vec(&message).unwrap();
@@ -109,21 +120,61 @@ fn handle_client(mut stream: TcpStream)->  std::io::Result<()> {
 
     Ok(())
 }
-    fn main() {
-        let server = TcpListener::bind(LOCAL).expect("Listener failed to bind");
+
+
+use std::collections::HashMap;
+use warp::{
+    http::{Response, StatusCode},
+};
+
+#[derive(Deserialize, Serialize)]
+struct MyObject {
+    key1: String,
+    key2: u32,
+}
+
+#[tokio::main]
+async fn main() {
+    // POST /data
+    let data_route = warp::post()
+        .and(warp::path("data"))
+        .and(warp::body::json())
+        .and_then(handle_post);
+    
+    let query = warp::get()
+        .and(warp::path("load"))
+       .and(warp::body::json())
+        .and_then(load);
+        
+        warp::serve(data_route.or(query))
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+    
+}
+async fn load(my_data: Message) -> Result<impl warp::Reply, Infallible> {
+
+    println!("Received data: {:?}", my_data.vector);
+    
+    let mut total_orders=Vec::new();
+    let conn =sqlite::Connection::open("order.db").unwrap();
+    let query = "SELECT * FROM data";
+ let mut statement=conn.prepare(query).unwrap();
+  while let sqlite::State::Row= statement.next().unwrap(){
   
-    
-        for stream in server.incoming() {
-            
-            match stream {
-                Err(e)=> {eprintln!("failed: {}", e)}
-                Ok(streams) => {
-                    handle_client(streams);
-                }
-            } 
-    
-        }
-    
-    
-       
-    }
+  let orderid= statement.read(0).unwrap();
+  let checkin=statement.read(1).unwrap();
+  total_orders.push((orderid,checkin,false));
+  } ;
+
+  
+  let message = Message { vector:total_orders};
+ 
+
+     
+    Ok(warp::reply::json(&message))
+}
+async fn handle_post(my_data: Message) -> Result<impl warp::Reply, Infallible> {
+    println!("Received data: {:?}", my_data.vector);
+    handle_connection(my_data.vector.clone());
+    Ok(warp::reply::json(&my_data))
+}
